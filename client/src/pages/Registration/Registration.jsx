1c5d53c6034699a39215fb29412ac885fe2055d0
Shojol <shojolislam.webappick@gmail.com>
2025-01-30 18:49:09 +0600

    complete home page

--- client/src/pages/Registration/Registration.jsx
+++ client/src/pages/Registration/Registration.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import InputField from "../../components/InputField";
 import Toast from "../../components/Toast";
 
